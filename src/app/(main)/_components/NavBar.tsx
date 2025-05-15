26f0cbf0f4c58e8e6176ccc759ab49d205efd7fb
Damian Akpan <damiakpan@gmail.com>
2025-05-15 15:18:54 +0100

    Added logo
    Signed-off-by: Damian Akpan <damiakpan@gmail.com>

--- src/app/(main)/_components/NavBar.tsx
+++ src/app/(main)/_components/NavBar.tsx
@@ -12,7 +12,12 @@ const NavBar = async (): Promise<React.ReactElement> => {
   const user = await getCurrentUser();
 
   return (
-    <nav className="flex flex-row-reverse items-center justify-between w-full px-16 py-8">
+    <nav className="flex items-center justify-between w-full px-16 py-8">
+      <div className="flex items-center gap-4">
+        <Link href="/" className="text-2xl font-bold text-gray-500 hover:text-gray-700">
+          Kick Ads
+        </Link>
+      </div>
       <div className="flex items-center gap-2">
         {/* Disabling this feature for now */}
         {/* <Link href="/products" className="px-3 py-2 text-sm font-semibold rounded-lg text-gray-500 hover:bg-gray-200">

beff69c2b131bfe87586eac50b77c86b3b183996
Damian Akpan <damiakpan@gmail.com>
2025-05-18 14:47:34 +0000

    Update

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,31 +1,16 @@
-import { createClient } from "redis";
+import "server-only";
 
-const redis = createClient({
+import * as redis from "redis";
+
+const cache = redis.createClient({
   url: process.env.REDIS_URL,
-  socket: {
-    reconnectStrategy: (retries) => {
-      // Maximum retry delay of 3 seconds
-      return Math.min(retries * 50, 3000);
-    }
-  },
 });
 
-redis.on("error", (err) => console.error("Redis Client Error:", err));
+cache.on("error", (err) => console.error("Redis Client Error:", err));
 
-export const connectRedis = async () => {
-  if (!redis.isOpen) {
-    try {
-      await redis.connect();
-      console.log("Connected to Redis redis");
-    } catch (error) {
-      console.error("Error connecting to Redis", error);
-      throw error;
-    }
-  }
-  return redis;
-};
+(async () => {
+  await cache.connect();
+})();
 
-// Initialize connection
-connectRedis().catch(console.error);
 
-export default redis;
+export default cache;
